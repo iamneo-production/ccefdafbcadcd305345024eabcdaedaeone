b386c641de639a4651d10fac2e2cf88114c8aa74
Iamneo <techadmin@iamneo.ai>
2023-09-23 06:28:52 +0000

    Backup Commit

--- ec2_instance/test.tf
+++ ec2_instance/test.tf
@@ -11,7 +11,7 @@ provider "aws"{
     secret_key= "I1FpuZmRv+7+9Qyq2JJG6pP2uq+vL2lPcxEeQgz7"
 }
 resource "aws_instance" "ec2_instance"{
-    ami="ami-08abb3eeacc61972d "
+    ami="ami-08abb3eeacc61972d"
     instance_type="t2.micro"
 }
 output "public_ip" {
